aa672d451d7209ea6bbd288ab7e688a17483be10
siggi <siggi@siggis-iMac-2.local>
2020-08-27 17:59:19 +0000

    move create_user.php

--- account/app/definition.js
+++ account/app/definition.js
@@ -21,6 +21,11 @@ app.definition =
 			"display_title": true,
 			"content": [
 				{
+					"type": "content",
+					"id": "captcha_info",
+					"content": "This site is protected by reCAPTCHA and the Google <a href='https://policies.google.com/privacy'>Privacy Policy</a> and <a href='https://policies.google.com/terms'>Terms of Service</a> apply."
+				},
+				{
 					"type": "form",
 					"id": "user",
 					"title": "Create Account",
